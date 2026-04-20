f3c582bc2d9f9ce8e166cfcd9c211f511b15e051
Your Name <you@example.com>
2026-04-19 21:46:45 -0400

    UserData

--- src/app/(private)/products/page.tsx
+++ src/app/(private)/products/page.tsx
@@ -1,7 +1,24 @@
+import { getLoggedinUser } from "@/app/server-actions/user";
+import { IUser } from "@/interfaces";
+import { redirect, RedirectType } from 'next/navigation'
+
 import React from "react";
 
-function ProductsPage() {
-  return <div>ProductsPage</div>;
+async function ProductsPage() {
+  const userResponse = await getLoggedinUser();
+  if (!userResponse.success){
+    console.log(userResponse.message);
+    redirect("/login")
+  }
+  const user:IUser = userResponse.data;
+  console.log(user)
+    console.log(user.email)
+
+  return <div>
+    <h1 >User: {user.name}</h1>
+        <h1>Email: {user.email}</h1>
+
+  </div>;
 }
 
 export default ProductsPage;
--- src/app/server-actions/user.ts
+++ src/app/server-actions/user.ts
@@ -2,11 +2,11 @@
 
 import { IUser } from "@/interfaces";
 import UserModel from "../models/user-model";
-import { email } from "zod";
+import { email, success } from "zod";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Cookies from 'js-cookie'
-
+import { cookies } from "next/headers";
 export const registerUser = async (payload: Partial<IUser>) => {
   try {
     const userExists = await UserModel.findOne({ email: payload.email });
@@ -27,7 +27,7 @@ export const loginUser = async (payload: Partial<IUser>) => {
     const userExists = await UserModel.findOne({ email: payload.email });
     if (!userExists) throw new Error("User does not exists");
 
-    const isMatch = await bcrypt.compare(payload.password!, userExists.password  );
+    const isMatch = await bcrypt.compare(payload.password!, userExists.password);
 
     if (!isMatch) {
       throw new Error("Password don't match");
@@ -39,13 +39,31 @@ export const loginUser = async (payload: Partial<IUser>) => {
         email: userExists.email
       },
       process.env.JWT_SECRET!,
-      {expiresIn: "1d"}
+      { expiresIn: "1d" }
     );
     console.log(token)
-      return { success: true, message: "Successful" , data: token};
-       
-    
+    return { success: true, message: "Successful", data: token };
+
+
   } catch (error: any) {
     return { success: false, message: error.message };
   }
 };
+
+
+export const getLoggedinUser = async () => {
+  try {
+    const cookieStore = cookies();
+    const token = (await cookieStore).get("token")?.value;
+    if (!token) throw new Error("Invalid authentication");
+    const decryptedData = await jwt.verify(token, process.env.JWT_SECRET!)
+
+    const { _id, email } = decryptedData as { _id: string, email: string };
+    const user = await UserModel.findOne({ email });
+    if (!user) throw new Error("Invalid user");
+    return { success: true, message: "user fetched" , data: user}
+  } catch (error:any) {
+    return { success: false, message: error.message };
+  }
+
+}
